0dc586b353decdef88055b30a286dd943cc61ea6
Jiahua2333 <ljh15088131916@gmail.com>
2020-03-09 18:56:13 -0400

    Add files via upload

--- LABs/Lab2/animation.js
+++ LABs/Lab2/animation.js
@@ -1,80 +1,94 @@
 "use strict";
 
 var Scene = {
-    canvas : undefined,
-    canvasContext : undefined,
+	canvas: undefined,
+	canvasContext: undefined,
 	sprite: undefined
 };
 
 Scene.start = function () {
 	// Get the canvas and it's context.
-    Scene.canvas = document.getElementById("myCanvas");
-    Scene.canvasContext = Scene.canvas.getContext("2d");
-	
-	// Seup the parrot to be displayed.
-    Scene.sprite = number;
-	
+	Scene.canvas = document.getElementById("myCanvas");
+	Scene.canvasContext = Scene.canvas.getContext("2d");
+
+	// Setup the number to be displayed.
+	Scene.sprite = number;
+
 	// Attach the image to be used for the sprite.
 	Scene.sprite.img = new Image();
-    Scene.sprite.img.src = Scene.sprite.src;
-	
-	// Wait till the parrot image is loaded before starting the animation.
-	Scene.sprite.img.onload = function() {		
-		Scene.sprite.offset=-Scene.sprite.frames[Scene.sprite.frame].frame.w;
-		//Scene.canvas.width = Scene.sprite.frames[1].frame.w + Scene.sprite.frames[0].frame.w
-    	Scene.mainLoop();
+	Scene.sprite.img.src = Scene.sprite.src;
+
+	// Set width of canvas to twice width of each number with same height
+	Scene.canvas.width = Scene.sprite.frames[Scene.sprite.frame].frame.w *2;
+	Scene.canvas.height = Scene.sprite.frames[Scene.sprite.frame].frame.h ;
+	// Set background color to be yellow
+	Scene.canvasContext.fillStyle = "yellow";
+	Scene.canvasContext.fillRect(0, 0, Scene.canvas.width , Scene.canvas.height);
+
+
+	// Wait till the number image is loaded before starting the animation.
+	Scene.sprite.img.onload = function () {
+		// Activate the button after images are ready.
+		document.getElementById("myBtn").disabled = false;
+
+		// Display 0 number image
+		Scene.sprite.frame = 0;
+		//img, x start, y start, w, h, x, y, w image use, h image use
+		Scene.canvasContext.drawImage(Scene.sprite.img, Scene.sprite.frames[Scene.sprite.frame].frame.x, Scene.sprite.frames[Scene.sprite.frame].frame.y, Scene.sprite.frames[Scene.sprite.frame].frame.w, Scene.sprite.frames[Scene.sprite.frame].frame.h, Scene.sprite.frames[Scene.sprite.frame].frame.w *0.5, 0, Scene.sprite.frames[Scene.sprite.frame].frame.w, Scene.sprite.frames[Scene.sprite.frame].frame.h);
+
+		// Once user click the button, check if the button shows start - start change number images
+		// if button shows others, reset image to number 0 and start change number images
+		document.getElementById("myBtn").addEventListener("click", function(){
+			if(document.getElementById("myBtn").value == "Start") {
+				Scene.mainLoop();
+			}	
+			else {
+				Scene.sprite.frame = 0;
+				Scene.clearCanvas();
+			} 
+				
+		});
+
+
 	}
 };
 
 // Once the basic HTML document is loaded and its parsing has taken place, start the scene.
-document.addEventListener( 'DOMContentLoaded', Scene.start);
+document.addEventListener('DOMContentLoaded', Scene.start);
 
+// Clear canvas with yellow color
 Scene.clearCanvas = function () {
-    Scene.canvasContext.fillStyle = "white";
-    Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
-	//document.write(Scene.canvas.width);
+	Scene.canvasContext.fillStyle = "yellow";
+	Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
 };
 
-Scene.mainLoop = function() {
-    Scene.clearCanvas();
-    Scene.update();
-    Scene.draw();
-	
-	// Animate at 24 frames a second.
-    window.setTimeout(Scene.mainLoop, 1000 /2);
+Scene.mainLoop = function () {
+	// Once image got change, disable button
+	document.getElementById("myBtn").disabled = true;
+	Scene.clearCanvas();
+	Scene.draw();
+
+	// Animate at 2 frames a second.
+	window.setTimeout(Scene.mainLoop, 1000 / 2);
 };
 
-Scene.update = function () {
-	// Set the canvas width to be that of the display Window. Which helps if you resize the window.
-  	Scene.canvas.width = window.innerWidth;
-	
-	// Set the location of the next frame. 
-  	Scene.sprite.offset+=24;
-	if(Scene.sprite.offset>Scene.canvas.width)
- 		Scene.sprite.offset=-Scene.sprite.frames[Scene.sprite.frame].frame.w;
-};
 
 Scene.draw = function () {
-	//Scene.canvas.width = Scene.sprite.img.width; 
-	//var h = Scene.canvas.height;
-	//Scene.canvas.height = Scene.sprite.img.height;
-	Scene.canvasContext.fillStyle = "blue";
-    //Scene.canvasContext.fillRect(0, Scene.canvas.height/2,Scene.sprite.img.width, Scene.sprite.img.height );
-	Scene.canvasContext.fillRect(0, 0,Scene.canvas.width, Scene.canvas.height);
-	if(Scene.sprite.frame < Scene.sprite.frames.length){
-		Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[Scene.sprite.frame].frame.x,Scene.sprite.frames[Scene.sprite.frame].frame.y,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h,Scene.sprite.offset,Scene.canvas.height/2,Scene.sprite.frames[Scene.sprite.frame].frame.w,Scene.sprite.frames[Scene.sprite.frame].frame.h);
+
+	// At the end of the sprite sheet, Display 10 with "1" and "0"
+	if (Scene.sprite.frame < Scene.sprite.frames.length) {
+		Scene.canvasContext.drawImage(Scene.sprite.img, Scene.sprite.frames[Scene.sprite.frame].frame.x, Scene.sprite.frames[Scene.sprite.frame].frame.y, Scene.sprite.frames[Scene.sprite.frame].frame.w, Scene.sprite.frames[Scene.sprite.frame].frame.h, Scene.sprite.frames[Scene.sprite.frame].frame.w *0.5, 0, Scene.sprite.frames[Scene.sprite.frame].frame.w, Scene.sprite.frames[Scene.sprite.frame].frame.h);
+		// Advance to the next frame.
+		Scene.sprite.frame++;
+
+	}
+	else {
+		Scene.canvasContext.drawImage(Scene.sprite.img, Scene.sprite.frames[1].frame.x, Scene.sprite.frames[1].frame.y, Scene.sprite.frames[1].frame.w, Scene.sprite.frames[1].frame.h, 0, 0, Scene.sprite.frames[1].frame.w, Scene.sprite.frames[1].frame.h);
+		Scene.canvasContext.drawImage(Scene.sprite.img, Scene.sprite.frames[0].frame.x, Scene.sprite.frames[0].frame.y, Scene.sprite.frames[0].frame.w, Scene.sprite.frames[0].frame.h, Scene.sprite.frames[0].frame.w-10, 0, Scene.sprite.frames[0].frame.w, Scene.sprite.frames[0].frame.h);
+		document.getElementById("myBtn").value="Restart";
+		document.getElementById("myBtn").disabled = false;
 	}
 	
-	// Advance to the next frame.
-	Scene.sprite.frame++;
-	
-	if(Scene.sprite.frame == Scene.sprite.frames.length + 1){
-		Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[1].frame.x,Scene.sprite.frames[1].frame.y,Scene.sprite.frames[1].frame.w,Scene.sprite.frames[1].frame.h,Scene.sprite.offset,Scene.canvas.height/2,Scene.sprite.frames[1].frame.w,Scene.sprite.frames[1].frame.h);
-		Scene.canvasContext.drawImage(Scene.sprite.img,Scene.sprite.frames[0].frame.x,Scene.sprite.frames[0].frame.y,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h,Scene.sprite.offset + Scene.sprite.frames[0].frame.w,Scene.canvas.height/2,Scene.sprite.frames[0].frame.w,Scene.sprite.frames[0].frame.h);
-		Scene.sprite.frame=0;
-	}
-/* 	// At the end of the sprite sheet, start at the first frame.
-	if(Scene.sprite.frame==Scene.sprite.frames.length)
-		Scene.sprite.frame=0; */
+
 };
 
--- LABs/Lab2/number.js
+++ LABs/Lab2/number.js
@@ -1,229 +1,238 @@
-number={
-	src : "spritesheet.png",
+number = {
+	src: "number.png",
 	img: undefined,
-	frame:0,
+	frame: 0,
 	offset: 0,
 	"frames": [
 		{
-			"filename": "tile000.png",
+			//"filename": "tile000.png",
+			"filename": "number0.png",
 			"frame": {
-				"x": 1,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 322,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 322,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile001.png",
+			//"filename": "tile001.png",
+			"filename": "number1.png",
 			"frame": {
-				"x": 123,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 6,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 6,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile002.png",
+			//"filename": "tile002.png",
+			"filename": "number2.png",
 			"frame": {
-				"x": 245,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 85,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 85,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile003.png",
+			//"filename": "tile003.png",
+			"filename": "number3.png",
 			"frame": {
-				"x": 367,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 164,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 164,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile004.png",
+			//"filename": "tile004.png",
+			"filename": "number4.png",
 			"frame": {
-				"x": 489,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 243,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 243,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile005.png",
+			//filename": "tile005.png",
+			"filename": "number5.png",
 			"frame": {
-				"x": 611,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 322,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 322,
+				"y": 4,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile006.png",
+			//"filename": "tile006.png",
+			"filename": "number6.png",
 			"frame": {
-				"x": 733,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 6,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 6,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile007.png",
+			//"filename": "tile007.png",
+			"filename": "number7.png",
 			"frame": {
-				"x": 855,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 85,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 85,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile008.png",
+			//"filename": "tile008.png",
+			"filename": "number8.png",
 			"frame": {
-				"x": 977,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 164,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 164,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
 		},
 		{
-			"filename": "tile009.png",
+			"filename": "number9.png",
 			"frame": {
-				"x": 1099,
-				"y": 1,
-				"w": 120,
-				"h": 175
+				"x": 243,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"rotated": false,
 			"trimmed": false,
 			"spriteSourceSize": {
-				"x": 0,
-				"y": 0,
-				"w": 120,
-				"h": 175
+				"x": 243,
+				"y": 158,
+				"w": 72,
+				"h": 144
 			},
 			"sourceSize": {
-				"w": 120,
-				"h": 175
+				"w": 72,
+				"h": 144
 			}
+
 		}
-		
 	],
 	"meta": {
 		"app": "http://www.codeandweb.com/texturepacker",
 		"version": "1.0",
-		"image": "spritesheet.png",
+		"image": "number.png",
 		"format": "RGBA8888",
 		"size": {
-			"w": 1220,
-			"h": 177
+			"w": 400,
+			"h": 307
 		},
 		"scale": "1"
 	}
